77dc27f380af8c9fbc7554cfa046577ad0e4c499
ささき <castanie96@gmail.com>
2022-06-03 12:52:35 +0900

    update

--- src/components/article/Comment/CommentForm.tsx
+++ src/components/article/Comment/CommentForm.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useRecoilValue } from 'recoil'
 import { accountState } from '@/lib/recoil'
 import { DisabledButton, ContainedButton } from '@/atoms/Button'
-import Form from '@/components/article/comment/Form'
+// import Form from '@/components/article/comment/Form'
 
 import styles from '@/styles/components/article/comment/commentForm.module.scss'
 import Skeleton from '@mui/material/Skeleton'
@@ -18,21 +18,21 @@ const CommentForm = () => {
   if(account.loading) return <Skeleton className={ styles.skeleton } variant='rectangular' />
 
   return (
-    <Form
-      text={ text }
-      setText={ setText }
-      name={ account.name }
-      placeholder='コメントする'
-    >
-      <div className={ styles.under }>
-        {/* 送信ボタン */}
-        { Boolean(text) ?
+    // <Form
+    //   text={ text }
+    //   setText={ setText }
+    //   name={ account.name }
+    //   placeholder='コメントする'
+    // >
+    //   <div className={ styles.under }>
+    //     {/* 送信ボタン */}
+    //     { Boolean(text) ?
           <ContainedButton text='送信' handle={ handlePost } />
-          :
-          <DisabledButton text='送信' />
-        }
-      </div>
-    </Form>
+    //       :
+    //       <DisabledButton text='送信' />
+    //     }
+    //   </div>
+    // </Form>
   )
 }
 
